ce7a3c436a564e51dd0828ef6eff0e08f373328d
Lisa Han <ellesis@gmail.com>
2024-09-21 15:54:45 -0700

    add flutter_svg library and svg files

--- lib/page_home.dart
+++ lib/page_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class PageHome extends StatefulWidget {
   const PageHome({super.key});
@@ -8,31 +9,46 @@ class PageHome extends StatefulWidget {
 }
 
 class _PageHomeState extends State<PageHome> {
+  Widget _appbarWidget() {
+    return AppBar(
+      title: GestureDetector(
+        onTap: () {
+          print('clicked tetle and arrow');
+        },
+        child: const Row(
+          children: [
+            Text('Home'),
+            Icon(Icons.arrow_drop_down),
+          ],
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.search),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.tune),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            "assets/svg/bell.svg",
+            width: 22,
+            height: 22,
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: GestureDetector(
-          onTap: () {
-            print('clicked tetle and arrow');
-          },
-          child: const Row(
-            children: [
-              Text('Home'),
-              Icon(Icons.arrow_drop_down),
-            ],
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.tune),
-          ),
-        ],
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: _appbarWidget(),
       ),
       body: Center(
         child: Text('Home'),
